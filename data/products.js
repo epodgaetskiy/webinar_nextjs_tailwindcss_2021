3bb57e1c391d15f39185263ca1e4a15df2a2f45c
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2021-07-28 20:05:47 +0300

    feat(main): products

---
+++ data/products.js
@@ -0,0 +1,68 @@
+export const products = [
+  {
+    id: "Apple-iPhone-11-64Gb-Black",
+    name: "Apple iPhone 11 64Gb Black (MHDA3) Slim Box",
+    price: "19 999",
+    feature: `
+      Материалы корпуса: Металл, Стекло
+      Влагозащита: IP68
+      Цвет: Черный
+      Тип экрана: IPS
+      Размер экрана: Безрамочный дисплей, 6,1
+    `,
+    imageUrl: "https://i.citrus.ua/imgcache/size_180/uploads/shop/d/d/ddf8b36b0bef85fc61453646a76fa934.jpg"
+  },
+  {
+    id: "Apple-iPhone-12-Pro-128GB-Pacific-Blue",
+    name: "Apple iPhone 12 Pro 128GB Pacific Blue",
+    price: "36 999",
+    feature: `
+      Материалы корпуса: Металл, Стекло
+      Влагозащита: IP68
+      Цвет: Синий
+      Тип экрана: OLED
+      Размер экрана: Безрамочный дисплей, 6,1"
+    `,
+    imageUrl: "https://i.citrus.ua/imgcache/size_180/uploads/shop/7/6/764f8071b21fa3d7b7430ba518665d4d.jpg"
+  },
+  {
+    id: "Apple-iPhone-12-128GB-Blue",
+    name: "Apple iPhone 12 64GB Blue",
+    price: "26 999",
+    feature: `
+      Материалы корпуса: Металл, Стекло
+      Влагозащита: IP68
+      Цвет: Синий
+      Тип экрана: OLED
+      Размер экрана: Безрамочный дисплей, 6,1"
+    `,
+    imageUrl: "https://i.citrus.ua/imgcache/size_180/uploads/shop/2/1/21c6dc6eebbbbdf5a291e66367f81acc.jpg"
+  },
+  {
+    id: "Apple-iPhone-SE-2020-64Gb-PRODUCT-Red",
+    name: "Apple iPhone SE 2020 64Gb PRODUCT Red",
+    price: "14 599",
+    feature: `
+      Характеристики
+      Материалы корпуса: Металл, Стекло
+      Влагозащита: IP67
+      Цвет: Красный
+      Тип экрана: IPS
+      Размер экрана: 4,7"
+    `,
+    imageUrl: "https://i.citrus.ua/imgcache/size_180/uploads/shop/2/5/2570b955dc4f9cdba9de5dc0786c7183.jpg"
+  },
+  {
+    id: "Apple-iPhone-11-64Gb-Purple",
+    name: "Apple iPhone 11 64Gb Purple",
+    price: "19 999",
+    feature: `
+      Материалы корпуса: Металл, Стекло
+      Влагозащита: IP68
+      Цвет: Фиолетовый
+      Тип экрана: IPS
+      Размер экрана: Безрамочный дисплей, 6,1"
+    `,
+    imageUrl: "https://i.citrus.ua/imgcache/size_180/uploads/shop/2/5/2570b955dc4f9cdba9de5dc0786c7183.jpg"
+  }
+]
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,45 @@
 import Head from 'next/head'
+import { products } from '../data/products';
+import { Rating } from '../components/Rating';
+
+const ProductCard = ({product}) => (
+  <div className="py-6">
+      <div className="max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+          <div className="flex justify-center">
+            <img src={product.imageUrl} alt="" />
+          </div> 
+          <div className="p-4">
+            <h1 className="text-gray-900 font-bold text-xl lg:text-2xl">{product.name}</h1>
+            <p className="mt-2 text-gray-600 text-sm"></p>
+            <div className="flex item-center mt-2">
+              <Rating />
+            </div>
+            <div className="flex item-center justify-between mt-3">
+              <h1 className="text-gray-700 font-bold text-xl">{product.price} грн</h1>
+              <button className="px-3 py-2 bg-yellow-600 text-white text-xs font-bold uppercase rounded">Купить</button>
+            </div>
+          </div>
+        </div>
+      </div>
+)
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+          <ProductCard product={products[0]} />
+          <ProductCard product={products[1]} />
+          <ProductCard product={products[2]} />
+          <ProductCard product={products[3]} />
+          <ProductCard product={products[4]} />
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
